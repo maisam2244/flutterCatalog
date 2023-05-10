9e67553603a81c9dfbe1504ef77756445243cfdf
Maisam Hussain <maisamlike@gmail.com>
2023-05-10 23:46:12 +0500

    day6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/my_routes.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,8 +23,8 @@ class MyApp extends StatelessWidget {
 
       ),
       routes: {
-        "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => Login(),
+        MyRoutes.homeRoute: (context) => HomePage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,50 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/my_routes.dart';
+import 'home_page.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+String name = "";
+bool changeState = false;
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset('assets/images/Login_Page.png', fit: BoxFit.cover),
-            const Text("Welcome",
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter username",
-                      labelText: "Username",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "Enter password",
-                      labelText: "Password",
-                    ),
-                  ),
-                ],
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset(
+            'assets/images/Login_Page.png',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Text(
+              "Welcome $name",
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(
-              height: 20.0,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                  ),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Enter password", labelText: "Password"),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
+                )
+              ],
             ),
-            ElevatedButton(
-              child: Text("Login"),
-              style: TextButton.styleFrom(),
-              onPressed: () {
-              print("Maisam kese ho");
-              },
-            )
-          ],
-        ));
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          //ElevatedButton(
+          //  style: TextButton.styleFrom(),
+          //  child: const Text("Sign in"),
+          //  onPressed: () {
+          //    Navigator.pushNamed(context, MyRoutes.homeRoute);
+          //  },
+          //)
+          InkWell(
+            onTap:  () async {
+              setState(() {
+                changeState = true;
+                //Navigator.pushNamed(context, MyRoutes.homeRoute);
+              });
+              await(Future.delayed(const Duration(seconds: 1 )));
+            },
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              height: 50,
+              width: changeState ? 70: 120,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: changeState? const Icon(Icons.done, color:  Colors.greenAccent)
+               : Text("Sign in",
+                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
